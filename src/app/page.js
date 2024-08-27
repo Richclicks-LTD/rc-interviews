39bfdb7e76be7adf11af2f1ede0e8644e403ed49
Mario <mario.arduini.gen@gmail.com>
2024-08-27 16:52:48 +0200

    v2.1

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,26 @@ export default function Home() {
     <main>
       <section className="bg-secondary">
         <div className="cont py-12">
-          <h2 className="h2 text-center">Ciao!</h2>
+          <h2 className="h2 text-center">Congratulations!</h2>
           <p className="mt-4">
-            Welcome to the coding part of the Richclicks interview, in this task
-            we are gonna code a little react component to show awesome perks in
-            working at Richclicks!
+            Welcome to the second coding part of the Richclicks interview, in
+            this task we are gonna create a slightly more elaborate component to
+            show the awesome perks in working at Richclicks!
           </p>
         </div>
       </section>
       <Perks />
+      <section className="bg-secondary">
+        <div className="cont py-12">
+          <h2 className="h2 text-center">Thank you for your time!</h2>
+          <p className="mt-4">
+            I am taking a little extra space here to personally thank you for
+            taking the time and effort to perform this task and to compliment
+            you for passing the first round. I am looking forward to seeing your
+            solutions and I hope you have fun with this task!
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
